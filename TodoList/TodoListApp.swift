5caa817d954f9b5b4bbda2b7bece323e3bb0dce3
Sams Tabrige <samsetabrige@yahoo.com>
2023-08-07 01:21:54 -0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Sams Tabrige on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
