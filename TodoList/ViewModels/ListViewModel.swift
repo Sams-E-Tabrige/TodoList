04eaf55adfd4e5b26c84e5672421321657945fe3
Sams Tabrige <samsetabrige@yahoo.com>
2023-08-10 05:44:51 -0700

    Save and persist data with UserDefault
    app is now able to save everything

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -7,21 +7,37 @@
 
 import Foundation
 
+/*
+ CRUD FUCNTIONS
+ Create
+ Read
+ Update
+ Delete
+ */
+
+
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    
+    let itemsKey: String = "items"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "This the the second!", isCompleted: true),
-            ItemModel(title: "Third!", isCompleted: false),
-        ]
-        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -31,4 +47,23 @@ class ListViewModel: ObservableObject {
     func moveItem(from: IndexSet, to: Int) {
         items.move(fromOffsets: from, toOffset: to)
     }
+    
+    func addItem(title: String){
+        let newItem = ItemModel(title: title, isCompleted: false)
+        items.append(newItem)
+    }
+    
+    func updateItem(item: ItemModel){
+        if let index = items.firstIndex(where: {  $0.id == item.id }){
+            items[index] = item.updateCompletion()
+        }
+        
+    }
+    
+    func saveItems() {
+        if let encodedDate = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedDate, forKey: itemsKey)
+        }
+    }
+    
 }
